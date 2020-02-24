89d3cce9fc4aa087ff8d7c42fa32d646acdd7277
spaceMan <yohhan@techcetra.com>
2020-02-24 15:02:54 +0530

    Fix

--- application/views/front/layout/header.php
+++ application/views/front/layout/header.php
@@ -11,13 +11,16 @@
                     <div class="col-md-12">
                         <nav class="navbar navbar-expand-lg">
                             <a class="navbar-brand" href="<?php echo site_url(); ?>"><img src="<?php echo base_url('resources/front/images/logo.png'); ?>" alt="logo"></a>
-                            <div class="input-group md-form form-sm form-2 pl-0">
-                                <input class="form-control my-0 py-1 amber-border" type="text" placeholder="Find Your Turf" aria-label="Search">
-                                <div class="input-group-append app">
-                                    <span class="input-group-text amber lighten-3" id="basic-text1"><i class="fa fa-search text-grey"
+                            <form action="<?php echo site_url('find-a-turf/grouped'); ?>">
+                                <input type="hidden" name="date" value="<?php echo date('Y-m-d') ?>">
+                                <div class="input-group md-form form-sm form-2 pl-0">
+                                    <input class="form-control my-0 py-1 amber-border" name="search" type="text" placeholder="Find Your Turf" aria-label="Search" value="<?php echo $this->input->get('search'); ?>">
+                                    <div class="input-group-append app">
+                                        <span class="input-group-text amber lighten-3" id="basic-text1"><i class="fa fa-search text-grey"
                                         aria-hidden="true"></i></span>
+                                    </div>
                                 </div>
-                            </div>
+                            </form>
                             <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
                                 <ul class="navbar-nav">
                                     <li class="nav-item active">

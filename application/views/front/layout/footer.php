fdd709e43d0103c0e9826a0ac9c813b1dad671ee
spaceMan <yohhan@techcetra.com>
2020-03-11 17:40:51 +0530

    Fix

--- application/views/front/layout/footer.php
+++ application/views/front/layout/footer.php
@@ -18,13 +18,15 @@
                         <a href="<?php echo site_url('privacy'); ?>">Privacy Policy</a>
                         <a href="<?php echo site_url('terms'); ?>">Terms & Conditions</a>
                     </div>
-                      <ul class="social-icons">
+                    <ul class="social-icons">
                         <li><a href="https://twitter.com/getonsyde"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
                         <li><a href="https://www.facebook.com/onsyde.in/?modal=admin_todo_tour"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
                         <li><a href="https://www.instagram.com/getonsyde/?hl=en"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
                         <li><a href="mailto:hello@onsyde.in"><i class="fa fa-envelope-o" aria-hidden="true"></i></a></li>
                         <li><a href="tel:+91 9372965837"><i class="fa fa-phone" aria-hidden="true"></i></a></li>
                     </ul>
+                    <hr>
+                    <p class="text-center">Made with <i class="fa fa-heart" style="color: #f18d8d;"></i> by <a target="_blank" href="http://www.thelazybaboons.com/"><img src="<?php echo base_url('resources/theme/images/TLB.svg'); ?>" width="24"></a></p>
                 </div>
             </div>
         </div>

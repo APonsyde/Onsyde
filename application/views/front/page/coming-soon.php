4d4f1fc2084fd3a8ce3d4609b33910f7dfaf4de1
tlbspaceman <yohhan@techcetra.com>
2020-04-16 14:26:34 +0530

    Fix

--- application/views/front/page/coming-soon.php
+++ application/views/front/page/coming-soon.php
@@ -2,6 +2,7 @@
 	<div class="container-fluid">
 		<div class="row justify-content-center">
 			<div class="col-md-12">
+				<img src="<?php echo base_url('resources/theme/images/no-turf-image.jpg'); ?>" class="mb-4">
 				<h3>Will be back soon!</h3>
 			</div>
 		</div>

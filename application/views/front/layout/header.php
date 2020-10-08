bbbb6b1ec93b1104edac30599960ed0f65d7a51f
Debajyoti Das <dev.debajyotidas@gmail.com>
2020-10-08 19:16:45 +0530

    Update homepage layout

--- application/views/front/layout/header.php
+++ application/views/front/layout/header.php
@@ -11,49 +11,6 @@
                     <div class="col-md-12">
                         <nav class="navbar navbar-expand-lg" style="min-height: 64px;">
                             <a class="navbar-brand" href="<?php echo site_url(); ?>"><img src="<?php echo base_url('resources/front/images/logo.png'); ?>" alt="logo"></a>
-                            <form action="<?php echo site_url('find-a-turf/grouped'); ?>" class="wid100">
-                                <input type="hidden" name="date" value="<?php echo date('Y-m-d'); ?>">
-                                <div class="input-group md-form form-sm form-2 pl-0">
-                                    <input class="form-control my-0 amber-border" type="text" name="search" placeholder="Find Your Turf" aria-label="Search">
-                                    <div class="input-group-append app">
-                                        <span class="input-group-text amber lighten-3" id="basic-text1"><i class="fa fa-search text-grey"
-                                            aria-hidden="true"></i></span>
-                                    </div>
-                                </div>
-                            </form>
-                            <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
-                                <?php if(!$this->session->userdata('player_id') && !$this->session->userdata('manager_id')) { ?>
-                                    <ul class="navbar-nav">
-                                        <li class="nav-item dropdown greenborder">
-                                            <a class="nav-link" href="<?php echo site_url('player'); ?>"  aria-haspopup="true" aria-expanded="false"> Player Login </a>
-                                        </li>
-                                    </ul>
-                                <?php } ?>
-                                <?php if($this->session->userdata('player_id') || $this->session->userdata('manager_id')) { ?>
-                                    <ul class="navbar-nav align-center">
-                                        <?php if($this->session->userdata('player_id')) { ?>
-                                            <li class="nav-item dropdown">
-                                                <a class="nav-link" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><span class="profilepic"> </span> <?php echo $this->session->userdata('player_name'); ?> <span class="ti-angle-down"></span></a>
-                                                <div class="dropdown-menu">
-                                                    <a class="dropdown-item" href="<?php echo site_url('bookings'); ?>">My Bookings</a>
-                                                    <a class="dropdown-item" href="<?php echo site_url('player/profile'); ?>">Profile</a>
-                                                    <a class="dropdown-item" href="<?php echo site_url('player/logout'); ?>">Logout</a>
-                                                </div>
-                                            </li>
-                                        <?php } ?>
-                                        <?php if($this->session->userdata('manager_id')) { ?>
-                                            <li class="nav-item dropdown">
-                                                <a class="nav-link" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><span class="profilepic"> </span> <?php echo $this->session->userdata('manager_name'); ?> <span class="ti-angle-down"></span></a>
-                                                <div class="dropdown-menu">
-                                                    <a class="dropdown-item" href="<?php echo site_url('manager/dashboard'); ?>">Dashboard</a>
-                                                    <a class="dropdown-item" href="<?php echo site_url('manager/profile'); ?>">Profile</a>
-                                                    <a class="dropdown-item" href="<?php echo site_url('manager/logout'); ?>">Logout</a>
-                                                </div>
-                                            </li>
-                                        <?php } ?>
-                                    </ul>
-                                <?php } ?>
-                            </div>
                         </nav>
                     </div>
                 </div>
--- application/views/front/page/home.php
+++ application/views/front/page/home.php
@@ -2,35 +2,53 @@
 <section class="hero-wrap d-flex align-items-centern turfBanner">
     <div class="swiper-container">
         <div class="swiper-wrapper">
-            <div class="swiper-slide"><img src="<?php echo base_url('resources/theme/images/l1.jpg'); ?>"></div>
-            <div class="swiper-slide"><img src="<?php echo base_url('resources/theme/images/l2.jpg'); ?>"></div>
-            <div class="swiper-slide"><img src="<?php echo base_url('resources/theme/images/l3.jpg'); ?>"></div>
+            <div class="swiper-slide"><img src="<?php echo base_url('resources/theme/images/banner.jpg'); ?>"></div>
         </div>
     </div>
     <div class="hero-title">
-        <h1>Find your next game</h1>
-        <form action="<?php echo site_url('find-a-turf/grouped'); ?>">
-            <input type="hidden" name="date" id="date" value="<?php echo $today; ?>">
-            <div class="search-box">
-                <div class="flexpanel">
-                    <div class="datepicker" style="background: #fff; cursor: pointer; padding: 5px 10px; width: 100%">
-                    <span></span> 
-                        <img src="<?php echo base_url('resources/front/images/calendar.svg'); ?>" alt="logo" class="calendar">
-                    </div>
-                    <div class="btn-search">
-                        <button class="btn btn-simple">Find Turfs →</button>
-                    </div>
-                </div>
-            </div>
-        </form>
+        <h1>Your Ultimate Sports Guide. Made for India</h1>
     </div>
 </section>
-<section class="main-block howit-work-wrap">
+<?php if(!empty($blogs)) { ?>
+  <section class="main-block">
+    <div class="container-fluid">
+      <div class="row justify-content-center">
+        <div class="col-md-10">
+          <div class="titile-block">
+            <h2 class="">Our Latest Posts</h2>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+          <?php foreach ($blogs as $key => $blog) { ?>
+            <div class="col-md-6 col-lg-3 article-first">
+              <a href="<?php echo site_url('blog/'.$blog['id']); ?>" class="blog2-link"><div class="news-block">
+                  <img src="<?php echo show_image(base_url('uploads/blogs/images/'.$blog['image'])); ?>" alt="#" class="img-fluid">
+                  <div class="news-title">
+                    <p><?php echo convert_db_time($blog['created_on'], "M d, Y") ?></p>
+                    <h5><?php echo $blog['title']; ?></h5>
+                    Read More ➝
+                  </div>
+                </div></a>
+            </div>
+          <?php } ?>
+      </div>
+      <div class="row justify-content-center">
+        <div class="col-md-12">
+          <div class="btn-wrap">
+            <a href="<?php echo site_url('blogs'); ?>" class="btn btn-simple">View All Posts →</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+<?php } ?>
+<section class="main-block howit-work-wrap gray">
     <div class="container-fluid">
         <div class="row justify-content-center">
             <div class="col-md-10">
                 <div class="titile-block">
-                    <h2>How Onsyde Works?</h2>
+                    <h2>What is Onsyde?</h2>
                     <p>Its really simple. Follow the steps and get started today!</p>
                 </div>
             </div>
@@ -44,30 +62,28 @@
             <div class="col-md-4">
                 <div class="howit-icon-wrap">
                     <div class="howit-img-block">
-                    <img src="<?php echo base_url('resources/theme/images/Turf.png'); ?>" alt="logo" class="">
+                    <img src="<?php echo base_url('resources/theme/images/discover.png'); ?>" alt="logo" class="">
                     </div>
-                    <h4>Your Turf</h4>
-                    <p>Find. Book. Play. No app to download.<span>No login required. It's that simple.</span></p>
+                    <h4>Discover</h4>
+                    <p>Reviews and recommendations of sports brands, products, media & more</p>
                 </div>
             </div>
             <div class="col-md-4">
                 <div class="howit-icon-wrap">
                     <div class="howit-img-block">
-                    <img src="<?php echo base_url('resources/theme/images/Team.png'); ?>" alt="logo" class="">
+                    <img src="<?php echo base_url('resources/theme/images/play.png'); ?>" alt="logo" class="">
                     </div>
-                    <h4>Your Team</h4>
-                    <p>Need a few players or a match with <span>
-                    your team? We help find your game</span></p>
+                    <h4>Play</h4>
+                    <p>Play our sports quiz every day & get a chance to win. Also play from our archive of 100+ quizzes</p>
                 </div>
             </div>
             <div class="col-md-4">
                 <div class="howit-icon-wrap">
                     <div class="howit-img-block">
-                    <img src="<?php echo base_url('resources/theme/images/Game.png'); ?>" alt="logo" class="">
+                    <img src="<?php echo base_url('resources/theme/images/learn.png'); ?>" alt="logo" class="">
                     </div>
-                    <h4>Your Game</h4>
-                    <p>Engage and share your moments of the <span>
-                    game with the world</span></p>
+                    <h4>Learn</h4>
+                    <p>Read our stories that inspire, educate and celebrate the love of the game</p>
                 </div>
             </div>
         </div>
@@ -79,19 +95,16 @@
             <div class="row marg0">
                 <div class="col-md-6 padding0">
                     <div class="turfManager contact-form">
-                        <h3>Your Sports Venue Partner</h3>
+                        <h3>Play The Onsyder Quiz</h3>
                         <ul class="sport">
-                            <li>Manage multiple bookings daily</li>
-                            <li>Reduce calls & cancellations </li>
-                            <li>Increase turf exposure & boost sales</li>
+                            <li>Get Smarter With Our Daily Quiz</li>
+                            <li>Earn Points and Climb the Leaderboard</li>
+                            <li>Top Players Get A Chance To Win</li>
                         </ul>
-                        <form action="<?php echo site_url('demo-ajax'); ?>" id="managerForm" method="post">
+                        <form action="<?php echo site_url('quiz'); ?>" id="managerForm" method="get">
                             <div class="submitForm flexpanel">
-                                <div class="form-group">
-                                    <input type="text" class="form-control" name="mobile" placeholder="Enter Your Mobile Number">
-                                </div>
-                                <div class="subBtn">
-                                    <a href="javascript:void(0)" id="submitManagerForm">Request Demo →</a>
+                                <div class="subBtn w-100">
+                                    <a href="javascript:void(0)" id="submitManagerForm">Play Now →</a>
                                 </div>
                             </div>
                             <div class="error"></div>
@@ -99,66 +112,46 @@
                     </div>
                 </div>
                 <div class="col-md-6">
-                    <figure class="pos"><img src="<?php echo base_url('resources/theme/images/Turf Pic.jpg'); ?>" class="wid100"></figure>
+                    <figure class="pos"><img src="<?php echo base_url('resources/theme/images/quiz.png'); ?>" class="wid100"></figure>
                 </div>
             </div>
         </div>
     </div>
 </section>
-<?php if(!empty($blogs)) { ?>
-    <section class="main-block">
-        <div class="container-fluid">
-            <div class="row justify-content-center">
-                <div class="col-md-10">
-                    <div class="titile-block">
-                        <h2 class="leftLine">Our Blogs</h2>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <?php foreach ($blogs as $key => $blog) { ?>
-                    <div class="col-md-6 col-lg-3 article-first">
-                    <a href="<?php echo site_url('blog/'.$blog['id']); ?>" class="blog2-link"><div class="news-block">
-                            <img src="<?php echo show_image(base_url('uploads/blogs/images/'.$blog['image'])); ?>" alt="#" class="img-fluid">
-                            <div class="news-title">
-                                <p><?php echo convert_db_time($blog['created_on'], "M d, Y") ?></p>
-                                <h5><?php echo $blog['title']; ?></h5>
-                               Read More ➝
-                            </div>
-                        </div></a>
-                    </div>
-                <?php } ?>
-            </div>
-            <div class="row justify-content-center">
-                <div class="col-md-12">
-                    <div class="btn-wrap">
-                        <a href="<?php echo site_url('blogs'); ?>" class="btn btn-simple">View All Blogs →</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-<?php } ?>
 <section class="main-block <?php echo !empty($blogs) ? 'gray' : ''; ?>">
     <div class="container-fluid">
         <div class="row justify-content-center">
-            <div class="col-md-10">
+          <div class="col-md-10">
+            <div class="titile-block">
+              <h2 class="">Subscribe</h2>
+            </div>
+            <div class="row">
+              <div class="col-md-6">
                 <div class="titile-block">
-                    <h2 class="rightLine">Podcast</h2>
-                    <p class="mb-4">Listen to conversations from <span class="bld">founders</span>, <span class="bld">personalities</span> and <span class="bld">fans</span> who live and breathe the game</p>
+                  <p class="mb-4">Listen to conversations from <span class="bld">founders</span>, <span class="bld">personalities</span> and <span class="bld">fans</span> who live and breathe the game</p>
                 </div>
                 <div class="flexpanel">
-                    <ul class="podcast flexpanel flex-wrap justify-content-center wid100 p-0">
-                        <?php foreach ($podcasts as $key => $podcast) { ?>
-                            <li>
-                                <a href="<?php echo $podcast['url']; ?>" target="_blank"><img src="<?php echo show_image(base_url('uploads/podcasts/images/'.$podcast['image'])); ?>">
-                                    <span><?php echo $podcast['title']; ?></span>
-                                </a>
-                            </li>
-                        <?php } ?>
-                    </ul>
+                  <ul class="podcast flexpanel flex-wrap justify-content-center wid100 p-0 bg-white py-3">
+                      <?php foreach ($podcasts as $key => $podcast) { ?>
+                        <li>
+                          <a href="<?php echo $podcast['url']; ?>" target="_blank"><img src="<?php echo show_image(base_url('uploads/podcasts/images/'.$podcast['image'])); ?>">
+                            <span><?php echo $podcast['title']; ?></span>
+                          </a>
+                        </li>
+                      <?php } ?>
+                  </ul>
                 </div>
+              </div>
+              <div class="col-md-6">
+                <div class="titile-block mt-3">
+                  <p class="mb-4">Our flagship <span class="bld">sport-focused</span> newsletter</p>
+                </div>
+                <div class="flexpanel mt-5">
+                  <iframe src="https://onsyde.substack.com/embed" width="480" height="330" style="border:1px solid #EEE; background:white;" frameborder="0" scrolling="no"></iframe>
+                </div>
+              </div>
             </div>
+          </div>
         </div>
     </div>
 </section>
@@ -201,6 +194,18 @@
     </div>
 </section>
 
+<style>
+  .howit-img-block {
+    overflow: hidden;
+  }
+  .pos {
+    top: 0px;
+  }
+  .pos img {
+    height: 100%;
+    object-fit: cover;
+  }
+</style>
 <script>
     $(document).ready(function() {
         var swiper = new Swiper('.swiper-container', {
